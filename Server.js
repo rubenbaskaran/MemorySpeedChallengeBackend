bb5d7c4da2fd276034a6984e45d8561f96b1fb50
Ruben Baskaran <rbaskaran92@gmail.com>
2018-08-01 11:11:03 +0200

    Added routes for getting and posting highscore

--- Server.js
+++ Server.js
@@ -1,2 +1,32 @@
-/* MemorySpeedChallengeBackend */
+var express = require("express");
+var bodyParser = require("body-parser");
 
+var app = express();
+app.use(bodyParser.json());
+
+app.listen(3000, () =>
+{
+    console.log("Started on port 3000");
+});
+
+app.get("/highscore", function (req, res)
+{
+    res.send({
+        firstplace: "1000",
+        secondplace: "800",
+        thirdplace: "500"
+    });
+})
+
+app.post("/highscore", function (req, res)
+{
+    var name = req.body.name;
+    var score = req.body.score;
+    console.log("Name: " + name + " and score: " + score)
+    res.status(200).send();
+})
+
+app.get("*", function (req, res)
+{
+    res.send("This is the default route");
+})
